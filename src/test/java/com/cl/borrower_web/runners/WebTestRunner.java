26ab7a348d61e24e47b2d226c87c6bcbbaa5acfb
satish302758 <57985553+satish302758@users.noreply.github.com>
2020-04-21 14:24:07 +0530

    added back same code

--- src/test/java/com/cl/borrower_web/runners/WebTestRunner.java
+++ src/test/java/com/cl/borrower_web/runners/WebTestRunner.java
@@ -4,9 +4,12 @@ import com.cl.java_af.testbases.CucumberTestBase;
 
 import cucumber.api.CucumberOptions;
 
-@CucumberOptions(features = { "src/test/resources/features/web/" }, glue = "com.cl.borrower_web.steps", plugin = { "pretty",
+@CucumberOptions(features = { "src/test/resources/features/web" }, glue = "com.cl.borrower_web.steps", plugin = { "pretty",
 		"json:target/cucumber-reports/CucumberTestReport.json",
 		"rerun:target/cucumber-reports/rerun.txt" }, monochrome = true, strict = false, dryRun = false)
 
+
+///transferFunds
+///payBills
 public class WebTestRunner extends CucumberTestBase {
 }
